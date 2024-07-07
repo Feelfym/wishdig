b86f4ab432dfa4af29fbfd392237bb171c60a8e3
Feelfym <tomoya.noguchi@gmail.com>
2024-07-07 18:00:04 +0900

    Basic認証の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   before_action :go_to_tutorial, only: [:index, :show]
+  before_action :basic_auth
 
   def after_sign_in_path_for(resource)
     items_path
@@ -22,4 +23,11 @@ class ApplicationController < ActionController::Base
       redirect_to step1_tutorial_path
     end
   end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+    end
+  end
+
 end
