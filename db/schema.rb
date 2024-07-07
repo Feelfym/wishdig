9bada7e396908d9453abb39a81a1aa7a56234964
Feelfym <tomoya.noguchi@gmail.com>
2024-07-07 17:29:08 +0900

    アプリ名の変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_06_30_081451) do
-  create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
+  create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_30_081451) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", charset: "utf8mb4", force: :cascade do |t|
+  create_table "active_storage_blobs", charset: "utf8", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -33,13 +33,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_30_081451) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", charset: "utf8mb4", force: :cascade do |t|
+  create_table "active_storage_variant_records", charset: "utf8", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "comparisons", charset: "utf8mb4", force: :cascade do |t|
+  create_table "comparisons", charset: "utf8", force: :cascade do |t|
     t.integer "primary_item_id"
     t.integer "secondary_item_id"
     t.datetime "created_at", null: false
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_30_081451) do
     t.integer "user_id"
   end
 
-  create_table "items", charset: "utf8mb4", force: :cascade do |t|
+  create_table "items", charset: "utf8", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "price"
@@ -60,7 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_30_081451) do
     t.date "purchased_date"
   end
 
-  create_table "memos", charset: "utf8mb4", force: :cascade do |t|
+  create_table "memos", charset: "utf8", force: :cascade do |t|
     t.text "content"
     t.integer "item_id"
     t.datetime "created_at", null: false
@@ -68,7 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_30_081451) do
     t.integer "title_id"
   end
 
-  create_table "notes", charset: "utf8mb4", force: :cascade do |t|
+  create_table "notes", charset: "utf8", force: :cascade do |t|
     t.bigint "comparison_id", null: false
     t.string "attribute_name"
     t.string "primary_value"
@@ -78,7 +78,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_30_081451) do
     t.index ["comparison_id"], name: "index_notes_on_comparison_id"
   end
 
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+  create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
